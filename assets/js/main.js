471389fe787c396d36e6ba33379dc845613225e0
Haynton <anthony.quenet@icloud.com>
2026-01-25 11:02:25 +0100

    Fix: content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 import initAnimations from "./components/animation.js";
-import initLangSwitch from "./components/lang.js";
 import initActiveLink from "./components/nav.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   initAnimations();
   initActiveLink();
-  initLangSwitch;
 });
